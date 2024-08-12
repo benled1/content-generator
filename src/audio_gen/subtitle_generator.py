62385fbc7f67afa22a62363fc1ff17815ee11e6d
Ben Ledingham <benledingham1@gmail.com>
2024-08-12 06:44:03 +0000

    start subtitle gen

--- src/audio_gen/subtitle_generator.py
+++ src/audio_gen/subtitle_generator.py
@@ -1,7 +1,16 @@
-
+from faster_whisper import WhisperModel
 
 
 class SubtitleGenerator():
 
     def __init__(self) -> None:
         pass
+
+    def transcribe_audio(self, audio_filepath, output_filepath) -> None:
+        model = WhisperModel("small")
+        segments, info = model.transcribe(audio_filepath, word_timestamps=True)
+
+        language = info.language
+        segments = list(segments)
+
+        
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
-from audio_gen import tts_generator
+from audio_gen.tts_generator import TTSGenerator 
+from audio_gen.subtitle_generator import SubtitleGenerator
+
 
 if __name__ == "__main__":
-    tts_gen = tts_generator.TTSGenerator("./local_media_store/audio")
-    tts_gen.generate_audio_file("Once upon a time, in a world filled with holographic screens and virtual reality, there lived a young girl named Zara. She was part of Generation Alpha, the first generation to grow up with AI assistants and self-driving cars. Zara was curious, always tinkering with gadgets and exploring new apps that let her experience different realities.")
+    # tts_gen = tts_generator.TTSGenerator("./local_media_store/audio")
+    # tts_gen.generate_audio_file("Once upon a time, in a world filled with holographic screens and virtual reality, there lived a young girl named Zara. She was part of Generation Alpha, the first generation to grow up with AI assistants and self-driving cars. Zara was curious, always tinkering with gadgets and exploring new apps that let her experience different realities.")
+    subtitle_generator = SubtitleGenerator()
+    subtitle_generator.transcribe_audio("./local_media_store/audio/test_audio.wav")
 
 
 
