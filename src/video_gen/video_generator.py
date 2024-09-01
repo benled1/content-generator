adcbfb80347d9687c22fcca94a6bf5db1c86e0a3
Ben Ledingham <benledingham1@gmail.com>
2024-09-01 01:16:33 +0000

    improve code quality

--- src/video_gen/video_generator.py
+++ src/video_gen/video_generator.py
@@ -5,6 +5,10 @@ class VideoGenerator():
     def __init__(self):
         pass
 
-
     def create_video(self, subtitle_file: str, audio_file: str, video_file: str):
+        """
+        combine the audio, video, and the subtitle files into a single .mp4 file.
+        """
+        # 
+        
         pass
