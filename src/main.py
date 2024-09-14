04b1196703f2885bfafccf0185a9da3460d19ada
Ben Ledingham <benledingham1@gmail.com>
2024-09-14 23:15:28 +0000

    forgot to leave notes to myself

--- src/main.py
+++ src/main.py
@@ -8,6 +8,13 @@ from media.subtitle.subtitle_maker import SubtitleMaker
 from media.subtitle.subtitle import Subtitle
 import uuid
 
+"""
+TODO:
+- create a footage generator/maker class that will retrieve or gen footage in some way. abstract this out in the same way it was done for both subtitles and audio
+- abstract away the method of uploading from the generator class. Generator should make and return the object with a reference to a local tmp file. Uploader/StorageManager should store the file
+    in a more official place like s3 or a none tmp file like (local_media_store)
+"""
+
 
 if __name__ == "__main__":
     
